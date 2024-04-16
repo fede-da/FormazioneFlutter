8c80aa86de3a01f36eae71f2f4fe73b31b913c71
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-04-16 20:15:38 +0200

    Convertito in ConsumerWidget

--- flutter_calendar/lib/feature/calendar/presentation/pages/calendar_page.dart
+++ flutter_calendar/lib/feature/calendar/presentation/pages/calendar_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar/feature/calendar/domain/models/meeting.dart';
 import 'package:flutter_calendar/feature/calendar/presentation/widgets/meeting_container.dart';
@@ -6,12 +5,26 @@ import 'package:flutter_calendar/feature/calendar/presentation/widgets/sfcalenda
 import 'package:flutter_calendar/feature/state/meeting_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//TODO: da convertire in ConsumerWidget e nno usare direttamente Consumer
-class MyHomePage extends StatelessWidget {
+// Consumer: è utilizzato per accedere ai valori forniti da un provider
+// senza dover ripetere manualmente la sua costruzione in ogni widget figlio.
+// Accetta una funzione di creazione del widget come parametro builder,
+// che viene chiamata ogni volta che il valore del provider cambia.
+// Questo widget è particolarmente utile quando si lavora con provider come Riverpod p
+// er gestire lo stato globale dell'applicazione.
+
+/// `ConsumerWidget` è un widget che permette di ascoltare i cambiamenti di un provider
+/// ricostruendo l'interfaccia utente in risposta a questi cambiamenti.
+/// Estende `StatelessWidget` e aggiunge un parametro extra al metodo `build`: l'oggetto "ref".
+/// Questo oggetto "ref" permette di interagire con i provider all'interno dell'albero dei widget.
+
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Meeting>? meetingsOnSelectedDate =
+        ref.watch(meetingProvider).meetings;
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -26,42 +39,30 @@ class MyHomePage extends StatelessWidget {
               children: [
                 Expanded(
                   flex: 1,
-                  // Consumer: è utilizzato per accedere ai valori forniti da un provider
-                  // senza dover ripetere manualmente la sua costruzione in ogni widget figlio.
-                  // Accetta una funzione di creazione del widget come parametro builder,
-                  // che viene chiamata ogni volta che il valore del provider cambia.
-                  // Questo widget è particolarmente utile quando si lavora con provider come Riverpod p
-                  // er gestire lo stato globale dell'applicazione.
-                  child: Consumer(builder: (context, ref, _) {
-                    return SfCalendarComponent(
-                      // Passa ref come parametro al widget SfCalendarComponent
-                      ref: ref,
-                      // Passa una funzione di callback per ricevere la lista degli appuntamenti del giorno selezionato
-                      onAppointmentsSelected: (meetings) {
-                        // Utilizza Riverpod per aggiornare lo stato
-                        ref
-                            .read(meetingProvider.notifier)
-                            .updateMeetings(meetings);
-                      },
-                    );
-                  }),
+                  child: SfCalendarComponent(
+                    // Passa ref come parametro al widget SfCalendarComponent
+                    ref: ref,
+                    // Passa una funzione di callback per ricevere la lista degli appuntamenti del giorno selezionato
+                    onAppointmentsSelected: (meetings) {
+                      // Utilizza Riverpod per aggiornare lo stato
+                      ref
+                          .read(meetingProvider.notifier)
+                          .updateMeetings(meetings);
+                    },
+                  ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Consumer(builder: (context, ref, _) {
-                    final List<Meeting>? meetingsOnSelectedDate =
-                        ref.watch(meetingProvider).meetings;
-                    return meetingsOnSelectedDate != null &&
-                            meetingsOnSelectedDate.isNotEmpty
-                        ? ListView(
-                            children:
-                                MeetingContainer.asList(meetingsOnSelectedDate),
-                          )
-                        : const Padding(
-                            padding: EdgeInsets.all(10),
-                            child: Text("Nessun evento attualmente"),
-                          );
-                  }),
+                  child: meetingsOnSelectedDate != null &&
+                          meetingsOnSelectedDate.isNotEmpty
+                      ? ListView(
+                          children:
+                              MeetingContainer.asList(meetingsOnSelectedDate),
+                        )
+                      : const Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text("Nessun evento attualmente"),
+                        ),
                 ),
               ],
             ),
