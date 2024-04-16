d27e1d1b251b21fbb1ebddfdb40382e239f70c7d
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-04-16 16:12:39 +0200

    Aggiunto stile per la prova

--- flutter_calendar/lib/feature/calendar/presentation/widgets/meeting_container.dart
+++ flutter_calendar/lib/feature/calendar/presentation/widgets/meeting_container.dart
@@ -12,10 +12,31 @@ class MeetingContainer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // Costruzione del container contenente il nome dell'evento.
-    return SizedBox(
-      width: double.infinity,
-      height: 100,
-      child: Text(meeting.eventName),
+    // return SizedBox(
+    //   width: double.infinity,
+    //   height: 100,
+    //   child: Text(meeting.eventName),
+    // );
+    return Card(
+      color: meeting.background,
+      elevation: 4, // Altezza dell'ombra della card
+      shape: RoundedRectangleBorder(
+        // Forma della card
+        borderRadius: BorderRadius.circular(8), // Bordo arrotondato
+      ),
+      child: SizedBox(
+        width: double.infinity, // Larghezza per riempire tutto lo schermo
+        height: 60, // Altezza fissa a 60
+        child: Center(
+          child: Text(
+            meeting.eventName,
+            style: const TextStyle(
+              fontSize: 16, // Dimensione del carattere
+              fontWeight: FontWeight.bold, // Grassetto
+            ),
+          ),
+        ),
+      ),
     );
   }
 
