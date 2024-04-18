e125fbc9dfc4bd8d96b5423a9eca5d87dc4a372d
kaguyasama <fede97d@hotmail.it>
2024-04-19 00:16:53 +0200

    TO complete

--- flutter_calendar/lib/feature/calendar/presentation/pages/calendar_page.dart
+++ flutter_calendar/lib/feature/calendar/presentation/pages/calendar_page.dart
@@ -28,24 +28,25 @@ class MyHomePage extends ConsumerWidget {
     final List<Meeting> meetingsOnSelectedDate =
         ref.watch(meetingsOnSelectedDateProvider);
     //TODO: controllare qui:
-    void addMeeting() {
+    void addMeeting(
+        /* prende come input il DateTime creato dall'utente in add_meeting_button*/) {
       // Ottieni la data selezionata dallo stato del provider
-      final DateTime selectedDate =
-          ref.read(meetingProvider.notifier).selectedDate;
-      final DateTime startTime =
-          DateTime(selectedDate.year, selectedDate.month, selectedDate.day, 9);
-      final DateTime endTime = startTime.add(const Duration(hours: 2));
+      // final DateTime selectedDate =
+      //     ref.read(meetingProvider.notifier).selectedDate;
+      // final DateTime startTime =
+      //     DateTime(selectedDate.year, selectedDate.month, selectedDate.day, 9);
+      // final DateTime endTime = startTime.add(const Duration(hours: 2));
 
-      Meeting newMeeting = Meeting(
-        'Nuovo Meeting ${meetingsOnSelectedDate.length ?? 0 + 1}',
-        startTime,
-        endTime,
-        Colors.purpleAccent,
-        false,
-      );
-      print("click =>");
-      print(newMeeting.toString());
-      ref.read(meetingProvider.notifier).addMeeting(newMeeting);
+      // Meeting newMeeting = Meeting(
+      //   'Nuovo Meeting ${meetingsOnSelectedDate.length ?? 0 + 1}',
+      //   startTime,
+      //   endTime,
+      //   Colors.purpleAccent,
+      //   false,
+      // );
+      // print("click =>");
+      // print(newMeeting.toString());
+      // ref.read(meetingProvider.notifier).addMeeting(newMeeting);
     }
 
     int aboveSize = 3;
--- flutter_calendar/lib/feature/calendar/presentation/widgets/add_meeting_button.dart
+++ flutter_calendar/lib/feature/calendar/presentation/widgets/add_meeting_button.dart
@@ -18,4 +18,22 @@ class AddMeetingButton extends StatelessWidget {
       ),
     );
   }
+
+  void showPopupSelectDayAndTime() {
+    final DateTime selectedDate = DateTime.now();
+    final DateTime startTime =
+        DateTime(selectedDate.year, selectedDate.month, selectedDate.day, 9);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+    DateTime selectedDateTime = DateTime.now();
+
+    // Conferma che aggiunge al provider
+  }
+}
+
+class DateTimeForMeeting {
+  final DateTime startTime;
+  final DateTime endTime;
+
+  DateTimeForMeeting({required this.startTime, required this.endTime});
 }
